3d5d13d050f24ab123df50a4130e3be5241cc293
deveshSAINI012 <161104117+deveshSAINI012@users.noreply.github.com>
2025-03-11 13:05:33 +0530

    Add files via upload

---
+++ binarysearch.cpp
@@ -0,0 +1,74 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(vector<int>& nums, int target) {
+    int n = nums.size(); //size of the array
+    int low = 0, high = n - 1;
+
+    // Perform the steps:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return mid;
+        else if (target > nums[mid]) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> a = {3, 4, 6, 7, 9, 12, 16, 17};
+    int target = 6;
+    int ind = binarySearch(a, target);
+    if (ind == -1) cout << "The target is not present." << endl;
+    else cout << "The target is at index: "
+                  << ind << endl;
+    return 0;
+}
+
+//Recursive
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(vector<int>& nums, int low, int high, int target) {
+
+    if (low > high) return -1; //Base case.
+
+    // Perform the steps:
+    int mid = (low + high) / 2;
+    if (nums[mid] == target) return mid;
+    else if (target > nums[mid])
+        return binarySearch(nums, mid + 1, high, target);
+    return binarySearch(nums, low, mid - 1, target);
+}
+
+int search(vector<int>& nums, int target) {
+    return binarySearch(nums, 0, nums.size() - 1, target);
+}
+
+int main()
+{
+    vector<int> a = {3, 4, 6, 7, 9, 12, 16, 17};
+    int target = 6;
+    int ind = search(a, target);
+    if (ind == -1) cout << "The target is not present." << endl;
+    else cout << "The target is at index: "
+                  << ind << endl;
+    return 0;
+}
+
+//TC= O(log n) base 2;
+
+
+
+//Lowerbound- smallest index in a sorted array such that arr[index]>=n,
+//n is the given target
+//in c++ we have-
+lb=lower_bound(arr.begin(),arr.end(),n);//gives an itrator pointing to that 
+// to find index-
+index=lb-arr.begin();
+//TC= O(log n) base 2;
+
+//Upperbound- smallest index in a sorted array such that arr[index]>n ,but not equal to;
